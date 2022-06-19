bd0ba1d3e7ea0fc4838474b69b6a50e9f05bf23d
hugodevops <felomeno8@gmail.com>
2022-06-19 17:39:48 -0300

    Servidor http

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+var http = require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at http://localhost:8081/');
