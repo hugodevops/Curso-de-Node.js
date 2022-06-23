e0eae6ff9b381c8cacbd137d7c2268dd0cc0956c
hugodevops <felomeno8@gmail.com>
2022-06-22 23:49:06 -0300

    Parametros

--- Index.js
+++ Index.js
@@ -10,7 +10,12 @@ app.get('/sobre', function(req, res){
 })
 
 app.get('/blog', function(req, res){
-    res.send('Bem vindo ao meu blog!')
+    res.send('Bem vindo ao meu blog!');
+})
+
+app.get('/Ola/:nome/:cargo/:cor', function(req, res){
+    res.send('<h1>Ola '+ req.params.nome +'</h1>'+'<h2> Seu cargo e: '+req.params.cargo+'</h2>'+'<h3> Sua cor favorita e: '+req.params.cor+'</h3>');
+    
 })
 
 app.listen(8081, function(){
